632923cde45905cfb551a9b932b99835033f22e7
Sebastian Fritzsche <sebifr58@gmx.de>
2023-12-19 20:41:44 +0100

    make number system + different lengths

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -33,6 +33,7 @@ namespace PasswortGenerator
             double maxLength = lenghtSlider.Value;
 
             char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w' , 'x', 'y' };
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             Random random = new Random();
 
 
@@ -62,7 +63,9 @@ namespace PasswortGenerator
                         }
                         else
                         {
-                            resultBlock.Text = allText + "1";
+                            int numberIndex = random.Next(0, 9);
+                            resultBlock.Text = allText + numbers[numberIndex];
+                           
                         }
 
                         allText = resultBlock.Text;
@@ -70,22 +73,178 @@ namespace PasswortGenerator
                    
                     break;
                 case 9:
-                    resultBlock.Text = "maxLength = 9";
+                    for (int i = 0; i <= 9; i++)
+                    {
+                        int letterOrNumber = random.Next(1, 20);
+
+                        if (letterOrNumber < 10)
+                        {
+                            int letterIndex = random.Next(0, 25);
+                            char letter = alphabet[letterIndex];
+
+                            int upOrLowIndex = random.Next(0, 10);
+
+                            if (upOrLowIndex <= 5)
+                                resultBlock.Text = allText + letter.ToString().ToUpper();
+
+                            else
+                                resultBlock.Text = allText + letter.ToString().ToLower();
+
+                        }
+                        else
+                        {
+                            int numberIndex = random.Next(0, 9);
+                            resultBlock.Text = allText + numbers[numberIndex];
+
+                        }
+
+                        allText = resultBlock.Text;
+                    }
                     break;
                 case 10:
-                    resultBlock.Text = "maxLength = 10";
+                    for (int i = 0; i <= 10; i++)
+                    {
+                        int letterOrNumber = random.Next(1, 20);
+
+                        if (letterOrNumber < 10)
+                        {
+                            int letterIndex = random.Next(0, 25);
+                            char letter = alphabet[letterIndex];
+
+                            int upOrLowIndex = random.Next(0, 10);
+
+                            if (upOrLowIndex <= 5)
+                                resultBlock.Text = allText + letter.ToString().ToUpper();
+
+                            else
+                                resultBlock.Text = allText + letter.ToString().ToLower();
+
+                        }
+                        else
+                        {
+                            int numberIndex = random.Next(0, 9);
+                            resultBlock.Text = allText + numbers[numberIndex];
+
+                        }
+
+                        allText = resultBlock.Text;
+                    }
                     break;
                 case 11:
-                    resultBlock.Text = "maxLength = 11";
+                    for (int i = 0; i <= 11; i++)
+                    {
+                        int letterOrNumber = random.Next(1, 20);
+
+                        if (letterOrNumber < 10)
+                        {
+                            int letterIndex = random.Next(0, 25);
+                            char letter = alphabet[letterIndex];
+
+                            int upOrLowIndex = random.Next(0, 10);
+
+                            if (upOrLowIndex <= 5)
+                                resultBlock.Text = allText + letter.ToString().ToUpper();
+
+                            else
+                                resultBlock.Text = allText + letter.ToString().ToLower();
+
+                        }
+                        else
+                        {
+                            int numberIndex = random.Next(0, 9);
+                            resultBlock.Text = allText + numbers[numberIndex];
+
+                        }
+
+                        allText = resultBlock.Text;
+                    }
                     break;
                 case 12:
-                    resultBlock.Text = "maxLength = 12";
+                    for (int i = 0; i <= 12; i++)
+                    {
+                        int letterOrNumber = random.Next(1, 20);
+
+                        if (letterOrNumber < 10)
+                        {
+                            int letterIndex = random.Next(0, 25);
+                            char letter = alphabet[letterIndex];
+
+                            int upOrLowIndex = random.Next(0, 10);
+
+                            if (upOrLowIndex <= 5)
+                                resultBlock.Text = allText + letter.ToString().ToUpper();
+
+                            else
+                                resultBlock.Text = allText + letter.ToString().ToLower();
+
+                        }
+                        else
+                        {
+                            int numberIndex = random.Next(0, 9);
+                            resultBlock.Text = allText + numbers[numberIndex];
+
+                        }
+
+                        allText = resultBlock.Text;
+                    }
                     break;
                 case 13:
-                    resultBlock.Text = "maxLength = 13";
+                    for (int i = 0; i <= 13; i++)
+                    {
+                        int letterOrNumber = random.Next(1, 20);
+
+                        if (letterOrNumber < 10)
+                        {
+                            int letterIndex = random.Next(0, 25);
+                            char letter = alphabet[letterIndex];
+
+                            int upOrLowIndex = random.Next(0, 10);
+
+                            if (upOrLowIndex <= 5)
+                                resultBlock.Text = allText + letter.ToString().ToUpper();
+
+                            else
+                                resultBlock.Text = allText + letter.ToString().ToLower();
+
+                        }
+                        else
+                        {
+                            int numberIndex = random.Next(0, 9);
+                            resultBlock.Text = allText + numbers[numberIndex];
+
+                        }
+
+                        allText = resultBlock.Text;
+                    }
                     break;
                 case 14:
-                    resultBlock.Text = "maxLength = 14";
+                    for (int i = 0; i <= 14; i++)
+                    {
+                        int letterOrNumber = random.Next(1, 20);
+
+                        if (letterOrNumber < 10)
+                        {
+                            int letterIndex = random.Next(0, 25);
+                            char letter = alphabet[letterIndex];
+
+                            int upOrLowIndex = random.Next(0, 10);
+
+                            if (upOrLowIndex <= 5)
+                                resultBlock.Text = allText + letter.ToString().ToUpper();
+
+                            else
+                                resultBlock.Text = allText + letter.ToString().ToLower();
+
+                        }
+                        else
+                        {
+                            int numberIndex = random.Next(0, 9);
+                            resultBlock.Text = allText + numbers[numberIndex];
+
+                        }
+
+                        allText = resultBlock.Text;
+                    }
                     break;
             }
 
